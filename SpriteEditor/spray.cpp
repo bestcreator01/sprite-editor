891095e0fe9d7b71a1e6694d2d369c5886a80c27
Seoin Kim <seoin.kim@utah.edu>
2023-11-12 20:41:20 -0700

    removed unnecessary codes

--- SpriteEditor/spray.cpp
+++ SpriteEditor/spray.cpp
@@ -7,10 +7,6 @@ Spray::Spray()
 
 void Spray::updatePixels(QImage &image, int x, int y, QRgb color)
 {
-    // calculated pixel coordinates from mouse position
-    int pixelX = (x - 180) / 20;
-    int pixelY = (y - 60) / 20;
-
     image.setPixel(x, y, color);
     image.setPixel(x, y, color);
     image.setPixel(x-1, y-1, color);
