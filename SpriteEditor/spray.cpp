7499bdae4523f66332fdf94e98412c8fc17948cf
Seoin Kim <seoin.kim@utah.edu>
2023-11-12 21:58:26 -0700

    removed duplicating codes

--- SpriteEditor/spray.cpp
+++ SpriteEditor/spray.cpp
@@ -8,11 +8,9 @@ Spray::Spray()
 void Spray::updatePixels(QImage &image, int x, int y, QRgb color)
 {
     image.setPixel(x, y, color);
-    image.setPixel(x, y, color);
     image.setPixel(x-1, y-1, color);
     image.setPixel(x-1, y, color);
     image.setPixel(x-2, y, color);
-    image.setPixel(x, y, color);
     image.setPixel(x+1, y+1, color);
     image.setPixel(x, y+1, color);
     image.setPixel(x-1, y+1, color);
